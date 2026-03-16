3eec48210fd7f075b93f950e0e002fb0b25e3c8b
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-03-16 08:58:11 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace matrixes_2
 {
     internal class Program
-<<<<<<< HEAD
+
     {
       
         static bool Palindrome_sum(int[,] matrix)
@@ -28,39 +28,6 @@
             }
             return false;
         }
-        static bool Edge_checker(int[,] matrix)
-        {
-          int sample = matrix[0,0];
-            
-   
-            for (int i = 0; i < matrix.GetLength(0); i++)
-=======
- {
-   
-     static bool Palindrome_sum(int[,] matrix)
-     {
-         bool loop=false;
-         int sum1 = 0, sum2 = 0,l=0,k=matrix.GetLength(0);
-         while(loop==false)
-         {
-             if (l > k)
-             {
-                 return true;
-             }
-             for(int i = 0; i < matrix.GetLength(1); i++)
-             {
-                 sum1 += matrix[l,i];
-                 sum2 += matrix[k,i];
-             }
-             if (sum1 != sum2)
-             {
-                 return false;
-             }
-             l++;
-             k--;
-         }
-         return false;
-     }
      static bool Edge_checker(int[,] matrix)
      {
          int sum = 0, sample = matrix[0,0],l=0,h=0,n;
@@ -120,62 +87,19 @@
              }
          }
             if(n==matrix.GetLength(0)* matrix.GetLength(1))
->>>>>>> 72bef5898f6adb3d9cac08ac8a8efe0925cb3034
+
             {
                  return true;
             }
-<<<<<<< HEAD
+
             return false;
         }
-        static bool Diagonal_sum(int[,] matrix)
-        {
-            int sum1 = 0, sum2 = 0;
-           
-            for (int i = matrix.GetLength(0); i >= 0; i--)
-            {
-                sum1 += matrix[i, i];
-            }
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                sum2 += matrix[i, i];
-            }
-            if (sum1 == sum2)
-            {
-                return true;
-            }
-            return false;
-        }
-        static bool Search_number(double[,] matrix, double min,double max)
-        {
-            int n = 0;
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (matrix[i, j] > min && matrix[i,j]<max)
-                    {
-                        n++;
-                    }
-                }
-            }
-               if(n==matrix.GetLength(0)* matrix.GetLength(1))
-               {
-                    return true;
-               }
-            return false;
-        }
+      
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
     }
-=======
-         return false;
-     }
-     static void Main(string[] args)
-     {
-         Console.WriteLine("Hello, World!");
-     }
- }
->>>>>>> 72bef5898f6adb3d9cac08ac8a8efe0925cb3034
+
+      
 }
